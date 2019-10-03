a59b6ab90cb02f76dd6085fc5927fc99df97b0ea
blitz2190 <blitz2190@DESKTOP-216274I>
2019-10-03 00:15:52 -0400

    asdasd

--- Data/Scripts/WeaponThread/script/Slave.cs
+++ Data/Scripts/WeaponThread/script/Slave.cs
@@ -200,6 +200,8 @@ namespace WeaponThread
             [ProtoMember(1)] internal string SubtypeId;
             [ProtoMember(2)] internal string AimPartId;
             [ProtoMember(3)] internal string MuzzlePartId;
+            [ProtoMember(4)] internal string AzimuthPartId;
+            [ProtoMember(5)] internal string ElevationPartId;
         }
 
         [ProtoContract]
